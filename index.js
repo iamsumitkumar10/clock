e4e410dcd9e41659a0c9d60d9955c7e1e86ee269
sumit kumar <sumitku3500@gmail.com>
2024-02-17 18:19:08 +0530

    modify day night icon

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ modeBtn.addEventListener("click", ()=> {
     
     if (currMode === "light"){
         currMode ="dark"; 
-         
         body.classList.add("dark"); 
         body.classList.remove("light");
     }
     else{
         currMode = "light";
-         
         body.classList.add("light");
         body.classList.remove("dark");
 
