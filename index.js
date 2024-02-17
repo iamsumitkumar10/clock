cb918044ca5166c3be05e129a6f43fda45d02be0
sumit kumar <sumitku3500@gmail.com>
2024-02-17 17:14:43 +0530

    add the theme mode

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 setInterval(showTime, 1000);
+
+let body = document.querySelector("body");
+let modeBtn = document.querySelector("#mode");
+let currMode = "light";
+
+
+modeBtn.addEventListener("click", ()=> {
+    
+    if (currMode === "light"){
+        currMode ="dark"; 
+         
+        body.classList.add("dark"); 
+        body.classList.remove("light");
+    }
+    else{
+        currMode = "light";
+         
+        body.classList.add("light");
+        body.classList.remove("dark");
+
+    }
+    console.log(currMode);
+});
+
+
 function showTime() {
 	let time = new Date();
 	let hour = time.getHours();
