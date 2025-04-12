a4671a81764ae9d3727d2320c2eec5e4d5e463e9
Sumit Kumar <sumitku3500@gmail.com>
2025-04-12 15:52:36 +0530

    change 24 hour formate

--- index.js
+++ index.js
@@ -19,28 +19,21 @@ modeBtn.addEventListener("click", () => {
     console.log("Theme:", currMode);
 });
 
+
+// 24 hours formate here 
 function showTime() {
     let time = new Date();
 
-    // Time
+    // Time (24-hour format)
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    let am_pm = "AM";
-
-    if (hour >= 12) {
-        am_pm = "PM";
-        if (hour > 12) hour -= 12;
-    }
-    if (hour == 0) {
-        hour = 12;
-    }
 
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
 
-    let currentTime = `${hour}:${min}:${sec} ${am_pm}`;
+    let currentTime = `${hour}:${min}:${sec}`;
     document.getElementById("clock").innerHTML = currentTime;
 
     // Date
@@ -58,6 +51,48 @@ function showTime() {
     let fullDate = `${day}, ${month} ${date}, ${year}`;
     document.getElementById("date").innerHTML = fullDate;
 }
+
+// 12 hours formate here 
+
+// function showTime() {
+//     let time = new Date();
+
+//     // Time
+//     let hour = time.getHours();
+//     let min = time.getMinutes();
+//     let sec = time.getSeconds();
+//     let am_pm = "AM";
+
+//     if (hour >= 12) {
+//         am_pm = "PM";
+//         if (hour > 12) hour -= 12;
+//     }
+//     if (hour == 0) {
+//         hour = 12;
+//     }
+
+//     hour = hour < 10 ? "0" + hour : hour;
+//     min = min < 10 ? "0" + min : min;
+//     sec = sec < 10 ? "0" + sec : sec;
+
+//     let currentTime = ${hour}:${min}:${sec} ${am_pm};
+//     document.getElementById("clock").innerHTML = currentTime;
+
+//     // Date
+//     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     const months = [
+//         "January", "February", "March", "April", "May", "June",
+//         "July", "August", "September", "October", "November", "December"
+//     ];
+
+//     let day = days[time.getDay()];
+//     let date = time.getDate();
+//     let month = months[time.getMonth()];
+//     let year = time.getFullYear();
+
+//     let fullDate = ${day}, ${month} ${date}, ${year};
+//     document.getElementById("date").innerHTML = fullDate;
+// }
 
 // Initialize time on load
 showTime();
